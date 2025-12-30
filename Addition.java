eb928ce1f55334025f5430b7857117a5098dd223
susir11 <susir.poudel91@gmail.com>
2025-12-30 23:17:54 +0545

    practice 1 : simple addition program

---
+++ Addition.java
@@ -0,0 +1,19 @@
+public class Addition {
+
+    
+    public static void main(String[] args) {
+
+       
+        int number1 = 10;
+
+      
+        int number2 = 20;
+
+        
+        int sum = number1 + number2;
+
+      
+        System.out.println("The sum is: " + sum);
+    }   
+
+}
